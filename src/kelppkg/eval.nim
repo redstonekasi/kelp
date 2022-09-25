7f0f1af95f5d81ffd5b7e25ec120171ab74615b1
redstonekasi <kasi@hanneskann.de>
2022-09-26 01:09:58 +0200

    [eval] no more segfaults

--- src/kelppkg/eval.nim
+++ src/kelppkg/eval.nim
@@ -29,7 +29,7 @@ proc quasiquote(node: KelpNode): KelpNode =
   else:
     node
 
-proc eval*(node: KelpNode, env: var KelpEnv): KelpNode
+proc eval*(node: KelpNode, env: var KelpEnv, depth = 0): KelpNode
 
 proc isMacroCall(node: KelpNode, env: KelpEnv): bool =
   if node.isList and node.list.len > 0 and node.list[0].isSymbol:
@@ -42,7 +42,7 @@ proc isMacroCall(node: KelpNode, env: KelpEnv): bool =
   #   let sym = env.get(node.list[0].str)
   #   return sym.isFun and sym.isMacro
 
-proc expand(node: KelpNode, env: KelpEnv): KelpNode =
+proc expand(node: KelpNode, env: KelpEnv, depth: int): KelpNode =
   result = node
   while result.isMacroCall(env):
     let fun = env.get(result.list[0].str)
@@ -54,33 +54,33 @@ proc expand(node: KelpNode, env: KelpEnv): KelpNode =
 
     var scope = createScope(fun, args)
 
-    result = fun.body.eval(scope).expand(env)
+    result = fun.body.eval(scope, depth + 1).expand(env, depth + 1)
 
-proc resolve(node: KelpNode, env: var KelpEnv): KelpNode =
+proc resolve(node: KelpNode, env: var KelpEnv, depth: int): KelpNode =
   case node.kind
   of kpSymbol:
     env.get(node.str)
   of kpList:
-    newList node.list.mapIt(it.eval(env))
+    newList node.list.mapIt(it.eval(env, depth + 1))
   of kpVector:
-    newVector node.list.mapIt(it.eval(env))
+    newVector node.list.mapIt(it.eval(env, depth + 1))
   of kpTable:
     let res = KelpNode(kind: kpTable)
     for k, v in node.table.pairs:
-      res.table[k] = v.eval(env)
+      res.table[k] = v.eval(env, depth + 1)
     res
   else:
     node
 
-proc eval*(node: KelpNode, env: var KelpEnv): KelpNode =
-  if getStackTraceEntries().len > 1500:
+proc eval*(node: KelpNode, env: var KelpEnv, depth = 0): KelpNode =
+  if depth > 1500:
     raise newException(EvalError, "too much recursion, try optimizing your code for tco")
 
   var node = node
   var env = env
 
   template default = # i hate this
-    let el = node.resolve(env)
+    let el = node.resolve(env, depth + 1)
     let fun = el.list[0]
     case fun.kind: # i can't use util#execute here because of circular dependencies
     of kpNative:
@@ -98,8 +98,8 @@ proc eval*(node: KelpNode, env: var KelpEnv): KelpNode =
       raise newException(EvalError, "expected first list item to be a function")
 
   while true:
-    node = node.expand(env)
-    if not node.isList: return node.resolve(env)
+    node = node.expand(env, depth + 1)
+    if not node.isList: return node.resolve(env, depth + 1)
     if node.list.len == 0: return node
 
     if node.list[0].isSymbol:
@@ -122,14 +122,14 @@ proc eval*(node: KelpNode, env: var KelpEnv): KelpNode =
         if key.str in SpecialForms:
           raise newException(EvalError, "cannot redefine special form")
 
-        return env.set(key.str, val.eval(env))
+        return env.set(key.str, val.eval(env, depth + 1))
       of "defmacro!":
         if node.list.len < 3:
           raise newException(EvalError, "insufficient amount of arguments, expected 2, got " & $(node.list.len - 1))
 
         let
           key = node.list[1]
-          val = node.list[2].eval(env)
+          val = node.list[2].eval(env, depth + 1)
 
         if not key.isSymbol:
           raise newException(EvalError, "expected key to be a symbol")
@@ -162,7 +162,7 @@ proc eval*(node: KelpNode, env: var KelpEnv): KelpNode =
           let v = binds.list[i + 1]
 
           if not k.isSymbol: raise newException(EvalError, "expected symbol as bind key")
-          scope.set(k.str, v.eval(scope))
+          scope.set(k.str, v.eval(scope, depth + 1))
         # for k, v in binds.table: # is there a way to convert an OrderedTable into a Table?
         #   scope.set(k, v.eval(scope))
 
@@ -191,14 +191,15 @@ proc eval*(node: KelpNode, env: var KelpEnv): KelpNode =
 
         node = newList(newSymbol("def!"), key, newList(newSymbol("fn*"), args, body))
       of "do":
-        discard newList(node.list[1..^1]).resolve(env) # evaluate all except last
+        if node.list.len > 2:
+          discard newList(node.list[1..^2]).resolve(env, depth + 1) # evaluate all except last
         node = node.list[^1] # eval and return last
       of "if":
         if node.list.len < 3:
           raise newException(EvalError, "insufficient amount of arguments, expected 2 or 3, got " & $(node.list.len - 1))
 
         let
-          cond = node.list[1].eval(env)
+          cond = node.list[1].eval(env, depth + 1)
           then = node.list[2]
 
         if cond.kind in {kpNil, kpFalse}: # only nil and false are falsey values
